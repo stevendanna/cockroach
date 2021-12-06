d92a76e707a204734b410d9e42e1a6e430eb6c39
Rui Hu <rui@cockroachlabs.com>
2021-12-06 02:24:16 -0500

    sql: add merging, remove conditional puts for delete-preserving indexes
    This change adds functionality to allow the schema changer to merge entries
    from a delete-preserving index to another index.
    
    This change also modifies the write path for delete-preserving indexes to
    use Put instead of CPut or InitPut as we don't need these types of checks
    for the delete-preserving indexes. The delete-preserving index will only
    be used as a log of writes during the new index backfilling process. Any
    constraints will be resolved when these entries are merged back into the main
    index.
    
    This change is part of a rewrite of the index backfiller:
    https://github.com/cockroachdb/cockroach/blob/master/docs/RFCS/20211004_incremental_index_backfiller.md#new-index-encoding-for-deletions-vs-mvcc
    
    Release note: None

--- pkg/sql/mvcc_backfiller.go
+++ pkg/sql/mvcc_backfiller.go
@@ -1,3 +1,13 @@
+// Copyright 2021 The Cockroach Authors.
+//
+// Use of this software is governed by the Business Source License
+// included in the file licenses/BSL.txt.
+//
+// As of the Change Date specified in that file, in accordance with
+// the Business Source License, use of this software will be governed
+// by the Apache License, Version 2.0, included in the file
+// licenses/APL.txt.
+
 package sql
 
 import (
@@ -7,23 +17,26 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/kv"
 	"github.com/cockroachdb/cockroach/pkg/roachpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/catalog"
-	"github.com/cockroachdb/cockroach/pkg/sql/catalog/descpb"
 	"github.com/cockroachdb/cockroach/pkg/sql/rowenc"
 	"github.com/cockroachdb/errors"
 )
 
+// Merge merges the entries in the destination index to the source index. The
+// destination index is always assumed to be using the delete-preserving
+// encoding. Key conflicts are resolved by always preferring the value in the
+// destination index, or by preferring the delete if applicable.
 func (sc *SchemaChanger) Merge(
 	ctx context.Context,
 	codec keys.SQLCodec,
 	table catalog.TableDescriptor,
-	source descpb.IndexID,
-	destination descpb.IndexID,
+	source catalog.Index,
+	destination catalog.Index,
 ) error {
-	// TODO: what's this supposed to be?
+	// TODO(rhu): Do we need to get the timestamp and use a fix timestamp?
 	mergeTimestamp := sc.clock.Now()
 
-	sourceSpan := table.IndexSpan(codec, source)
-	destSpan := table.IndexSpan(codec, destination)
+	sourceSpan := table.IndexSpan(codec, source.GetID())
+	destSpan := table.IndexSpan(codec, destination.GetID())
 
 	const pageSize = 1000
 
@@ -33,7 +46,8 @@ func (sc *SchemaChanger) Merge(
 		}
 
 		return txn.Iterate(ctx, sourceSpan.Key, sourceSpan.EndKey, pageSize, func(kvs []kv.KeyValue) error {
-			b := txn.NewBatch()
+			destKeys := make([]roachpb.Key, len(kvs))
+
 			prefixLen := len(sourceSpan.Key)
 
 			// For now just grab all of the destination KVs and merge the corresponding entries.
@@ -47,16 +61,12 @@ func (sc *SchemaChanger) Merge(
 				destKey := make([]byte, len(destSpan.Key))
 				copy(destKey, destSpan.Key)
 				destKey = append(destKey, sourceKV.Key[prefixLen:]...)
-				b.Get(destKey)
-			}
-
-			if err := txn.Run(ctx, b); err != nil {
-				return err
+				destKeys[i] = destKey
 			}
 
 			wb := txn.NewBatch()
 			for i := range kvs {
-				mergedEntry, deleted, err := mergeEntry(&kvs[i], &b.Results[i].Rows[0])
+				mergedEntry, deleted, err := mergeEntry(&kvs[i], destKeys[i])
 				if err != nil {
 					return err
 				}
@@ -73,30 +83,26 @@ func (sc *SchemaChanger) Merge(
 	})
 }
 
-func mergeEntry(sourceKV *kv.KeyValue, destKV *kv.KeyValue) (*kv.KeyValue, bool, error) {
+func mergeEntry(sourceKV *kv.KeyValue, destKey roachpb.Key) (*kv.KeyValue, bool, error) {
 	var destTagAndData []byte
 	var deleted bool
 
-	if !destKV.Exists() {
-		destTagAndData = sourceKV.Value.TagAndDataBytes()
-	} else {
-		tempWrapper, err := rowenc.DecodeWrapper(sourceKV.Value)
-		if err != nil {
-			return nil, false, err
-		}
+	tempWrapper, err := rowenc.DecodeWrapper(sourceKV.Value)
+	if err != nil {
+		return nil, false, err
+	}
 
-		if tempWrapper.Deleted {
-			deleted = true
-		} else {
-			destTagAndData = tempWrapper.Value
-		}
+	if tempWrapper.Deleted {
+		deleted = true
+	} else {
+		destTagAndData = tempWrapper.Value
 	}
 
 	value := &roachpb.Value{}
 	value.SetTagAndData(destTagAndData)
 
 	return &kv.KeyValue{
-		Key:   destKV.Key,
+		Key:   destKey,
 		Value: value,
 	}, deleted, nil
 }
